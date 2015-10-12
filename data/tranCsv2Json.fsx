2df9fac5897e01ab1dcf40923d914e8269868dd0
Bartul Bonacic <bartul.bonacic@trovicor.com>
2015-10-12 12:04:21 +0400

    Catalog test data

--- data/tranCsv2Json.fsx
+++ data/tranCsv2Json.fsx
@@ -1,23 +1,41 @@
 #r "./packages/FSharp.Data.2.2.5/lib/net40/FSharp.Data.dll"
 open FSharp.Data
+open System.IO
 
 type CsvBooks = CsvProvider<"catalog.books.csv", ";">
 let data = CsvBooks.Load("catalog.books.csv")
 
 type JsonBooks = JsonProvider<"catalog.books.example.json">
 
+
 let rows = 
     data.Rows 
-    |> Seq.take 3
+    |> Seq.take 100
 
-rows |> Seq.iter (fun x -> printfn "%s" (x.BookTitle + " " + x.GetType().Name))
+printfn "----------------------------------------"
+rows |> Seq.iter (fun x -> printfn "%s" x.BookTitle)
 
+//JsonValue.WriteTo(new StreamWriter(""), );
+
+printfn "----------------------------------------"
+let writer = new StreamWriter("./catalog.books.json");
 let jsons =
     rows 
     |> Seq.map (fun row ->
-        JsonValue.Record [|("ISBN", JsonValue.String row.ISBN); ("BookTitle", JsonValue.String row.BookTitle)|]
+            [|("ISBN", JsonValue.String row.ISBN); 
+            ("BookTitle", JsonValue.String row.BookTitle);
+            ("BookAuthor", JsonValue.String row.BookAuthor);
+            ("YearOfPublication", JsonValue.String(row.YearOfPublication.ToString()));
+            ("Publisher", JsonValue.String row.Publisher);
+            ("ImageUrlSmall", JsonValue.String row.ImageUrlSmall);
+            ("ImageUrlMedium", JsonValue.String row.ImageUrlMedium);
+            ("ImageUrlLarge", JsonValue.String row.ImageUrlLarge)|] |> JsonValue.Record 
         )
+    //|> Seq.iter (fun i -> printfn "%s" (i.ToString(JsonSaveOptions.DisableFormatting))) 
+    |> Seq.iter (fun i -> writer.WriteLine(i.ToString(JsonSaveOptions.DisableFormatting))) 
 
-//jsons |> Seq.iter (fun x -> printfn "%s" JsonValue.String)
 
+printfn "----------------------------------------"
 printf "Alo!"
+
+printfn "----------------------------------------"
