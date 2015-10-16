7f332eb36171c5f6d31f1da9ed8ed348ee990a32
Bartul Bonacic <bartul.bonacic@live.com>
2015-10-16 21:28:21 +0400

    text and isbn index in catalog db

--- dev/import_catalog.bat
+++ dev/import_catalog.bat
@@ -1,1 +1,3 @@
 mongoimport --db catalog --collection books --drop --file ./data/catalog.books.json
+mongo catalog --eval 'db.books.createIndex({ ISBN: 1 })'
+mongo catalog --eval 'db.books.createIndex({ BookTitle: "text" }, { name: "BookTextIndex"})'
