2b6a45e1c551a864d3a2f82ef45f96712b5474e5
Bartul Bonacic <bartul.bonacic@live.com>
2015-10-17 22:45:03 +0400

    request book component init

---
+++ Source/Books.Web/DemandTrack/RequestButtonViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Books.Web.DemandTrack
+{
+    public class RequestButtonViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string bookId)
+        {
+            return View(new RequestButtonViewModel { BookId = bookId });
+        }
+    }
+    public class RequestButtonViewModel
+    {
+        public string BookId { get; set; }
+    }
+}
